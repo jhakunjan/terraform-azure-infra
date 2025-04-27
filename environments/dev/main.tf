3391716bf9aa6d25125143ff5d9fc8fba77b948e
jhakunjan <jhakunjan2000@gmail.com>
2025-04-27 16:20:45 +0000

    added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,13 +11,24 @@ module "network" {
   nsg_name = "dev-nsg"
 }
 
+data "azurerm_key_vault" "dev_vault" {
+  name                =  var.vault_key_name
+  resource_group_name = var.resource_group_name   
+}
+
+data "azurerm_key_vault_secret" "vm_admin_password" {
+  name         = "vm3-admin-password"                
+  key_vault_id = data.azurerm_key_vault.dev_vault.id
+}
+
+
 module "compute" {
   source              = "../../modules/compute"
   resource_group_name = var.resource_group_name
   location            = var.location
   subnet_id           = module.network.subnet_ids["frontend"]
   admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  admin_password      = data.azurerm_key_vault_secret.vm_admin_password.value
 
   vm_map = {
     "dev-linux-vm"   = { os_type = "Linux", vm_size = "Standard_B1s" }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,3 +2,4 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "admin_username" {}
 variable "admin_password" {}
+variable "vault_key_name" {}
